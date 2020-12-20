bf4ff495cc8c811d154c6b0791596c3d550d30d8
Gabriel Rajendran <rgabriel1508@gmail.com>
2020-12-20 15:05:56 +0530

    Updated blockchain.js

--- server/blockchain/blockchain.js
+++ server/blockchain/blockchain.js
@@ -14,7 +14,7 @@ class Blockchain{
             "<no previous hash>",
             "genesis_hash",
             'genesis_nonce',
-            'genesis_difficulty'
+            2
         )
     }
 
@@ -23,8 +23,8 @@ class Blockchain{
     }
 
     addBlock(newBlock){
-        newBlock.prev_hash = this.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
+        newBlock.prevHash = this.getLatestBlock().hash
+        newBlock.hash = Block.calculateHash(newBlock.index, newBlock.timestamp, newBlock.data, newBlock.prevHash, newBlock.nonce, newBlock.difficulty);
         this.chain.push(newBlock) 
     }
 
@@ -33,11 +33,11 @@ class Blockchain{
             const currentBlock = this.chain[i]
             const previousBlock = this.chain[i-1]
 
-            if(currentBlock.hash !== currentBlock.calculateHash()){
+            if(currentBlock.hash !== Block.calculateHash(currentBlock.index, currentBlock.timestamp, currentBlock.data, currentBlock.prevHash, currentBlock.nonce, currentBlock.difficulty)){
                 return false
             }
 
-            if(currentBlock.prev_hash !== previousBlock.hash){
+            if(currentBlock.prevHash !== previousBlock.hash){
                 return false;
             }
         }
@@ -45,11 +45,17 @@ class Blockchain{
     }
 }
 
-let critCoin = new Blockchain()
-critCoin.addBlock(new Block(1,"18/12/2020",{"amount":4},0,0,0))
-critCoin.addBlock(new Block(2,"18/12/2020",{"amount":10},0,0,0))
+function test(){
+  let critCoin = new Blockchain()
+  critCoin.addBlock(new Block(1,"18/12/2020",{"amount":4},0,0,0))
+  critCoin.addBlock(new Block(2,"18/12/2020",{"amount":10},0,0,0))
 
-console.log("Is chain valid? "+ critCoin.isChainValid())
+  //uncomment next line to make chain invalid
+  //critCoin.chain[1].data = "bruh";
+  console.log("Is chain valid? "+ critCoin.isChainValid())
 
-console.log(JSON.stringify(critCoin, null, 4))
+  console.log(JSON.stringify(critCoin, null, 4))
+}
 
+//uncomment next line to test
+//test()
