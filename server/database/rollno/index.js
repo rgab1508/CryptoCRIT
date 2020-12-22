aca723a17378b8d3f0a1abf871774a207b3e9436
theabbie <abhishek7gg7@gmail.com>
2020-12-22 18:36:42 +0530

    only validate transaction part left

--- server/database/rollno/index.js
+++ server/database/rollno/index.js
@@ -1,6 +1,26 @@
 require('dotenv').config({path: './../../.env'});
 var DB = require("./../index.js");
 
+class Exception {
+  constructor(code,message) {
+    this.code = code;
+    this.message = message;
+  } 
+}
+
+class keypair {
+  constructor(public_key,private_key) {
+    this.public_key = public_key;
+    this.private_key = private_key
+  }
+  getPublic() {
+    return this.public_key;
+  }
+  getPrivate() {
+    return this.private_key;
+  }
+}
+
 var root = "cryptocrit/students";
 
 class Student {
@@ -12,11 +32,18 @@ class Student {
     return student.email;
   }
 
-  static async setKeyPair(rollno, keyPair) {
+  static async setPublicKey(rollno, public_key) {
     var db = new DB(root);
-    await db.write(rollno + "/public_key", keyPair.getPublic());
-    await db.write(rollno + "/private_key", keyPair.getPrivate());
+    await db.write(rollno + "/public_key", public_key);
     return true;
+  }
+
+  static async getPublicKey(rollno) {
+    var db = new DB(root);
+    var student = await db.read(rollno);
+    if (!student) throw new Exception(400,"Student doesn't exist");
+    if (student.public_key) return student.public_key;
+    else throw new Exception(500,"Student hasn't signed up yet");
   }
 
   static async exists(rollno) {
